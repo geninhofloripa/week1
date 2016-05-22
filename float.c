87d4119ca3f3fba136c1dec3b309fde883c0be5a
geninhofloripa <geninhofloripa@hotmail.com>
2016-05-22 12:38:35 -0300

    demonstrates how the value 0.01 is saved in a float variable

---
+++ float.c
@@ -0,0 +1,10 @@
+// This demonstrates how the value 0.01 is saved in a float variable
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("This demonstrates how the value 0.01 is saved in a float variable: \n");
+    float f = 0.01;
+    printf("%.50f\n", f);
+}
